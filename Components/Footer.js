9b72ae62feb82b3fc4e4c45dbef7924fcfa27b7c
Disha Swarup <77547235+Disha-swp@users.noreply.github.com>
2024-07-06 21:51:00 +0530

    Update Footer.js

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 function redirectToGitHub() {
+    console.log("i am clicked")
     window.location.href = "https://github.com/Disha-swp";
 }
 export default function Footer(){
